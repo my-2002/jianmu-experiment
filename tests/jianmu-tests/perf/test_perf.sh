ec5903cf4ceb8aae895d3f320f1c16307eeee2ec
syx <syx0511@mail.ustc.edu.cn>
2024-03-26 07:49:36 +0000

    perf sh文件修改

--- tests/jianmu-tests/perf/test_perf.sh
+++ tests/jianmu-tests/perf/test_perf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 project_dir=$(realpath ../../../)
-io_dir=$(realpath "$project_dir"/src/jainmu-src)
+io_dir=$(realpath "$project_dir"/src/jianmu-src)
 output_dir=output
 suffix=sy
 
@@ -38,20 +38,11 @@ for case in $testcases; do
     case_base_name=$(basename -s .$suffix "$case")
     in_file=$test_dir/$case_base_name.in
     asm_mem2reg_on=$output_dir/${case_base_name}-mem2reg-on.s
-    asm_mem2reg_off=$output_dir/${case_base_name}-mem2reg-off.s
+    
     exe_mem2reg_on=$output_dir/${case_base_name}-mem2reg-on
-    exe_mem2reg_off=$output_dir/${case_base_name}-mem2reg-off
+
 
     echo "==========$case=========="
-    #### mem2reg off
-    # cminusfc compile to .s
-    bash -c "cminusfc -S $case -o $asm_mem2reg_off" >>$LOG 2>&1
-    check_return_value $? 0 "CE" "cminusfc compiler error" || continue
-    # gcc compile asm to executable
-    loongarch64-unknown-linux-gnu-gcc -static \
-        "$asm_mem2reg_off" "$io_dir"/io.c -o "$exe_mem2reg_off" \
-        >>$LOG
-    check_return_value $? 0 "CE" "gcc compiler error" || continue
 
     #### mem2reg on
     # cminusfc compile to .s
@@ -59,17 +50,11 @@ for case in $testcases; do
     check_return_value $? 0 "CE" "cminusfc compiler error" || continue
     # gcc compile asm to executable
     loongarch64-unknown-linux-gnu-gcc -static \
-        "$asm_mem2reg_on" "$io_dir"/io.c -o "$exe_mem2reg_on" \
+        "$asm_mem2reg_on" "$io_dir"/sylib.c -o "$exe_mem2reg_on" \
         >>$LOG
     check_return_value $? 0 "CE" "gcc compiler error" || continue
 
-    echo "==========mem2reg off"
-    if [ -e "$in_file" ]; then
-        exec_cmd="qemu-loongarch64 $exe_mem2reg_off >/dev/null <$in_file"
-    else
-        exec_cmd="qemu-loongarch64 $exe_mem2reg_off >/dev/null"
-    fi
-    time bash -c "$exec_cmd"
+    
     echo "==========mem2reg on"
     if [ -e "$in_file" ]; then
         exec_cmd="qemu-loongarch64 $exe_mem2reg_on >/dev/null <$in_file"
